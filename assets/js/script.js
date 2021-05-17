eba05a67f3f3b093263f2f6be91aeb37daf5ccd8
Sam Morgen <samorgen14@gmail.com>
2021-05-17 14:25:16 -0700

    Resets form after submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ var taskFormHandler = function () {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
@@ -15,6 +21,7 @@ var taskFormHandler = function () {
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  formEl.reset();
 };
 
 var createTaskEl = function (taskDataObj) {
